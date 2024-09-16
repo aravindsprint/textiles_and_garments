3823397e31383072a5929d55be46daa60e3b21f2
Aravind <aravindsprint@gmail.com>
2024-09-16 16:39:06 +0530

    added the settings file

---
+++ textiles_and_garments/textiles_and_garments/doctype/textiles_and_garments_settings/textiles_and_garments_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Aravind and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Textiles and Garments Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
