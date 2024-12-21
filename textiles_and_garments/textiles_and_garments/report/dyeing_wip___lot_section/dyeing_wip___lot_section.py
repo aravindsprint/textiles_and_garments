1a0cd8b21ca2bf2e819afb09e90a3819933f76b8
Aravind <aravindsprint@gmail.com>
2024-12-21 11:54:56 +0530

    updated

--- textiles_and_garments/textiles_and_garments/report/dyeing_wip___lot_section/dyeing_wip___lot_section.py
+++ textiles_and_garments/textiles_and_garments/report/dyeing_wip___lot_section/dyeing_wip___lot_section.py
@@ -196,6 +196,8 @@ def get_stock_entry_detail_data_from_stock_entry(filters):
             AND wori.item_code = ste_entry_item.item_code  -- Match based on item_code
         WHERE 
             ste_entry_item.docstatus = 1
+            AND ste_entry_item.status NOT LIKE 'Cancelled'
+            AND ste_entry_item.status NOT LIKE 'Stopped'
             AND ste_entry_item.parent LIKE 'MT/%'
             AND ste_entry_item.item_code LIKE '%KF%/%'
             AND ste_entry_item.t_warehouse = 'DYE/LOT SECTION - PSS'
