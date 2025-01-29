d16c4fc0894f75cf062d8d91a4a4ac471f01acd5
Aravind <aravindsprint@gmail.com>
2025-01-29 12:38:40 +0530

    jobwork report

---
+++ textiles_and_garments/textiles_and_garments/report/jobwork_ageing_in_dyeing_unit/jobwork_ageing_in_dyeing_unit.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2025, Aravind and contributors
+// For license information, please see license.txt
+
+
+frappe.query_reports["Jobwork Ageing in Dyeing Unit"] = {
+	"filters": [
+		{
+			fieldname: "stock_entry",
+			label: __("Stock Entry"),
+			fieldtype: "Link",
+			width: "280",
+			options: "Stock Entry",
+		},
+		{
+			fieldname: "posting_date",
+			label: __("From This Date"),
+			fieldtype: "Date",
+			width: "80",
+			reqd: 1,
+			default: frappe.datetime.add_days(frappe.datetime.get_today(), -7),
+		},
+		{
+			fieldname: "posting_date",
+			label: __("To This Date"),
+			fieldtype: "Date",
+			width: "80",
+			reqd: 1,
+			default: frappe.datetime.get_today(),
+		},
+
+	]
+};
+
---
+++ textiles_and_garments/textiles_and_garments/report/jobwork_ageing_in_dyeing_unit/jobwork_ageing_in_dyeing_unit.py
@@ -0,0 +1,245 @@
+from collections import defaultdict
+import frappe
+from frappe import _
+from frappe.query_builder.functions import Sum
+from frappe.utils import flt, today
+
+from datetime import date
+from frappe import _
+
+# def execute(filters=None):
+#     columns, data = [], []
+#     data = get_data(filters)
+#     columns = get_columns(filters)
+#     return columns, data
+
+
+# def get_columns(filters):
+#     columns = []
+
+#     columns.extend(
+#         [
+#             {
+#                 "label": _("Date"),
+#                 "fieldname": "posting_date",
+#                 "fieldtype": "Date",
+#                 "width": 150,
+#             },
+#             {
+#                 "label": _("Stock Entry"),
+#                 "fieldname": "stock_entry",
+#                 "fieldtype": "Link",
+#                 "options": "Stock Entry",
+#                 "width": 200,
+#             },
+#             {
+#                 "label": _("Work Order"),
+#                 "fieldname": "work_order",
+#                 "fieldtype": "Link",
+#                 "options": "Work Order",
+#                 "width": 200,
+#             },
+#             {
+#                 "label": _("Ageing"),
+#                 "fieldname": "days",
+#                 "fieldtype": "Data",
+#                 "width": 150
+#             },
+#             {
+#                 "label": _("Status"),
+#                 "fieldname": "status",
+#                 "fieldtype": "Data",
+#                 "width": 150,
+#             }
+#         ]
+#     )
+
+#     return columns
+
+
+# def get_data(filters):
+#     data = []
+#     sales_data = get_data_from_stock_entry(filters)
+#     data.extend(sales_data)
+#     return data
+
+
+# def get_data_from_stock_entry(filters):
+#     # Base query to fetch data from Sales Invoice and Sales Invoice Item
+#     query = """
+#         SELECT 
+#             stock_entry.name AS stock_entry,
+#             stock_entry.custom_reference_dyeing_work_order AS work_order,
+#             stock_entry.posting_date AS posting_date,
+#             wo.status AS status
+#         FROM 
+#             `tabStock Entry` AS stock_entry
+#         LEFT JOIN 
+#             `tabWork Order` AS wo
+#         ON 
+#             stock_entry.custom_reference_dyeing_work_order = wo.name
+#         WHERE 
+#             stock_entry.docstatus = 1
+#             AND stock_entry.name LIKE %(item_code_pattern)s
+#     """
+
+    
+
+    
+#     # Dynamically append additional filter conditions
+#     conditions = []
+    
+#     if filters.get("item_code"):
+#         conditions.append("stock_entry.item_code = %(item_code)s")
+    
+#     if filters.get("batch_no"):
+#         conditions.append("stock_entry.batch_no = %(batch_no)s")
+    
+#     if filters.get("work_order"):
+#         conditions.append("wo.custom_work_order = %(work_order)s")
+    
+#     if filters.get("from_date"):
+#         conditions.append("wo.posting_date >= %(from_date)s")
+
+#     if filters.get("to_date"):
+#         conditions.append("wo.posting_date <= %(to_date)s")
+    
+#     if filters.get("status"):
+#         conditions.append("wo.status = %(status)s")
+    
+#     # If conditions were added, append them to the query
+#     if conditions:
+#         query += " AND " + " AND ".join(conditions)
+
+#     # Define filter values to pass as parameters
+#     filter_values = {
+#         # "item_code_pattern": 'JDI%/%',  # Passing the pattern as a parameter
+#         "item_code_pattern": 'MAT%',  # Passing the pattern as a parameter
+#         "from_date": filters.get("from_date"),
+#         "to_date": filters.get("to_date"),
+#         "status": filters.get("status")
+#     }
+    
+#     # Execute the query with filters
+#     return frappe.db.sql(query, filter_values, as_dict=1)
+
+
+def execute(filters=None):
+    columns, data = [], []
+    data = get_data(filters)
+    columns = get_columns(filters)
+    return columns, data
+
+def get_columns(filters):
+    columns = []
+
+    columns.extend(
+        [
+            {
+                "label": _("Date"),
+                "fieldname": "posting_date",
+                "fieldtype": "Date",
+                "width": 150,
+            },
+            {
+                "label": _("Stock Entry"),
+                "fieldname": "stock_entry",
+                "fieldtype": "Link",
+                "options": "Stock Entry",
+                "width": 200,
+            },
+            {
+                "label": _("Work Order"),
+                "fieldname": "work_order",
+                "fieldtype": "Link",
+                "options": "Work Order",
+                "width": 200,
+            },
+            {
+                "label": _("Ageing"),
+                "fieldname": "ageing_days",
+                "fieldtype": "Int",
+                "width": 150
+            },
+            {
+                "label": _("Status"),
+                "fieldname": "status",
+                "fieldtype": "Data",
+                "width": 150,
+            }
+        ]
+    )
+
+    return columns
+
+def get_data(filters):
+    data = []
+    sales_data = get_data_from_stock_entry(filters)
+    data.extend(sales_data)
+    return data
+
+def get_data_from_stock_entry(filters):
+    today = date.today()
+
+    # Base query to fetch data from Stock Entry and Work Order
+    query = """
+        SELECT 
+            stock_entry.name AS stock_entry,
+            stock_entry.custom_reference_dyeing_work_order AS work_order,
+            stock_entry.posting_date AS posting_date,
+            wo.status AS status
+        FROM 
+            `tabStock Entry` AS stock_entry
+        LEFT JOIN 
+            `tabWork Order` AS wo
+        ON 
+            stock_entry.custom_reference_dyeing_work_order = wo.name
+        WHERE 
+            stock_entry.docstatus = 1
+            AND stock_entry.name LIKE %(item_code_pattern)s
+    """
+
+    # Dynamically append additional filter conditions
+    conditions = []
+    
+    if filters.get("item_code"):
+        conditions.append("stock_entry.item_code = %(item_code)s")
+    
+    if filters.get("batch_no"):
+        conditions.append("stock_entry.batch_no = %(batch_no)s")
+    
+    if filters.get("work_order"):
+        conditions.append("wo.custom_work_order = %(work_order)s")
+    
+    if filters.get("from_date"):
+        conditions.append("stock_entry.posting_date >= %(from_date)s")
+
+    if filters.get("to_date"):
+        conditions.append("stock_entry.posting_date <= %(to_date)s")
+    
+    if filters.get("status"):
+        conditions.append("wo.status = %(status)s")
+    
+    # If conditions were added, append them to the query
+    if conditions:
+        query += " AND " + " AND ".join(conditions)
+
+    # Define filter values to pass as parameters
+    filter_values = {
+        "item_code_pattern": 'MAT%',  # Adjust pattern as needed
+        "from_date": filters.get("from_date"),
+        "to_date": filters.get("to_date"),
+        "status": filters.get("status")
+    }
+    
+    # Execute the query with filters
+    results = frappe.db.sql(query, filter_values, as_dict=1)
+
+    # Calculate Ageing Days
+    for row in results:
+        if row.get("posting_date"):
+            row["ageing_days"] = (today - row["posting_date"]).days
+        else:
+            row["ageing_days"] = None  # Handle missing dates
+
+    return results
