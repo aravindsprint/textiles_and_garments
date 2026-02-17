cacf391f90670b9525db978890c907fe729ae6d2
Aravind <aravindsprint@gmail.com>
2026-02-17 16:05:15 +0530

    hang tag

--- textiles_and_garments/textiles_and_garments/doctype/tags_name/test_tags_name.py
+++ textiles_and_garments/textiles_and_garments/doctype/tags_name/test_tags_name.py
@@ -1,9 +1,9 @@
-# Copyright (c) 2025, Aravind and Contributors
+# Copyright (c) 2026, Aravind and Contributors
 # See license.txt
 
 # import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestHangTagRequest(FrappeTestCase):
+class TestTagsName(FrappeTestCase):
 	pass
